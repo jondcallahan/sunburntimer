2a47a4ebef1b03ecea0cfc7eefd2c830d299e692
Jon Callahan <j@joncallahan.com>
2025-06-30 15:56:14 -0500

    Revert "fix: wrangler"
    This reverts commit 9002ada15b91cb9576fa5b700c7d9bdb027bbb56.

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,14 @@
-import path from "path";
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import { cloudflare } from "@cloudflare/vite-plugin";
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), cloudflare()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+})
