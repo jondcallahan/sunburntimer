80a4dcfbf8c635a390de7d0332fd509edd154b70
Jon Callahan <j@joncallahan.com>
2025-07-12 13:34:35 -0500

    chore: lints

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,13 @@ import "./index.css";
 import App from "./App.tsx";
 import { Analytics } from "@vercel/analytics/react";
 
-createRoot(document.getElementById("root")!).render(
+const root = document.getElementById("root");
+
+if (!root) {
+	throw new Error("Root element not found, check index.html!");
+}
+
+createRoot(root).render(
 	<StrictMode>
 		<App />
 		<Analytics />
